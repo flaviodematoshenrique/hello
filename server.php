ad6f70019dd79c9faf65b7ece760acd5b69bb2b1
FLAVIO DE MATOS HENRIQUE <63165435+flaviodematoshenrique@users.noreply.github.com>
2023-01-10 11:53:05 -0300

    Update server.php

--- server.php
+++ server.php
@@ -44,9 +44,9 @@ require_once __DIR__.'/public/index.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+/*$app = require_once __DIR__.'/../bootstrap/app.php';
 
-/*$kernel = $app->make(Kernel::class);
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
